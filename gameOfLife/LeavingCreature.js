0969d3671ace1f0bacb2d764a4f95ab5e61b8832
ZakaryanHayk <manvel.m.g@tumo.org>
2023-04-08 10:57:19 +0400

    JavaScript Class Inheritanace

---
+++ gameOfLife/LeavingCreature.js
@@ -0,0 +1,18 @@
+class LeavingCreauture{
+    constructor(){
+        this.x = x
+        this.y = y
+        this.multiply = 0
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x    , this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y    ],
+            [this.x + 1, this.y    ],
+            [this.x - 1, this.y + 1],
+            [this.x    , this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+}
+
